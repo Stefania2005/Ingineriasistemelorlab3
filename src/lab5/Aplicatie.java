884c9f9235e8984c6d16e9b7a5ca8154db605acc
Stefania Nadia <stefanianadia.stoian@ulbsibiu.ro>
2025-04-04 08:09:40 +0300

    lab3/4/5

--- src/lab5/Aplicatie.java
+++ src/lab5/Aplicatie.java
@@ -1,48 +1,60 @@
 package lab5;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
+
 public class Aplicatie {
     public static void main(String[] args) {
         try {
-            printSmallTextFile("in.txt");
+            String inputFile = "in.txt";
+            String outputFile = "out.txt";
+
+            printSmallTextFile(inputFile);
             System.out.println("\n\n");
-            //printLargerTextFile("in.txt");
+            addNewLineAfterDot(inputFile);
+
+            saveModifications(inputFile, outputFile);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    //sub a
+    // a) Adăugare newline după fiecare linie
     static void printSmallTextFile(String fileName) throws IOException {
         System.out.println("Using Files.readAllLines:");
         Path path = Paths.get(fileName);
         List<String> allLinesInMemory = Files.readAllLines(path);
-        for(String line: allLinesInMemory) {
-            System.out.println(line + "\n");
+        for (int i = 0; i < allLinesInMemory.size(); i++) {
+            allLinesInMemory.set(i, allLinesInMemory.get(i) + "\n");
+            System.out.print(allLinesInMemory.get(i));
         }
     }
 
-    //b
+    // b) Adăugare newline după fiecare punct
     static void addNewLineAfterDot(String fileName) throws IOException {
         Path path = Paths.get(fileName);
         List<String> allLinesInMemory = Files.readAllLines(path);
-        for (String line : allLinesInMemory) {
-            String modifiedLine = line.replace(".", ".\n");
-            System.out.println(modifiedLine);
-        }
-
-
-//        static void printLargerTextFile (String fileName) throws IOException {
-//            System.out.println("Using Scanner:");
-//            Path path = Paths.get(fileName);
-//            try (Scanner scanner = new Scanner(path)) {
-//                while (scanner.hasNextLine()) {
-//                    System.out.println(": " + scanner.nextLine());
-//                }
-//            }
+        for (int i = 0; i < allLinesInMemory.size(); i++) {
+            allLinesInMemory.set(i, allLinesInMemory.get(i).replace(".", ".\n"));
+            System.out.println(allLinesInMemory.get(i));
         }
     }
+
+    // c) Salvare în fișierul out.txt
+    static void saveModifications(String inputFile, String outputFile) throws IOException {
+        Path path = Paths.get(inputFile);
+        List<String> allLines = Files.readAllLines(path);
+
+        for (int i = 0; i < allLines.size(); i++) {
+            allLines.set(i, allLines.get(i).replace(".", ".\n") + "\n");
+        }
+
+        Files.write(Paths.get(outputFile), allLines);
+        System.out.println("Modificările au fost salvate în " + outputFile);
+    }
+}
